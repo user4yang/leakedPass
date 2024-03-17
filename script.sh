bdaa089dae5d043af25365501bad9c58c8948815
yang <linuxuseryangxyz@gmail.com>
2024-03-17 20:07:15 +0530

    second commit

--- script.sh
+++ script.sh
@@ -7,8 +7,14 @@ pkg install -y python
 echo "Installing Flask..."
 pip install flask
 
+echo "Installing Wget"
+pip install wget
+
+echo "Installing cloudflare"
+wget https://github.com/cloudflare/cloudflared/releases/download/2024.2.1/cloudflared-linux-arm64
+
 echo "Making cloudflared executable..."
-chmod +x cloudflared
+chmod +x cloudflared-linux-arm64
 
 # Start server
 echo "Starting server..."
@@ -16,4 +22,4 @@ python app.py &
 
 # Start Cloudflared tunnel
 echo "Starting Cloudflared tunnel..."
-./cloudflared tunnel --url 127.0.0.1:5000
+./cloudflared-linux-arm64 tunnel --url 127.0.0.1:5000
